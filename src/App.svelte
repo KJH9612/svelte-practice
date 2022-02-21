a62121b7732aa1ae6944d764cce18cdfe4512b0e
kjh9612 <hackertimoarm@gmail.com>
2022-02-21 15:30:02 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Header from './header.svelte'
+	import Content from './content.svelte'
+	import Footer from './footer.svelte'
+
+	let count = 0;
+	const handleClick = () => count = count + 1;
+</script>
+
+<Header />
+<Content />
+<Footer />
+<button on:click={handleClick}>
+	클릭수 {count}
+</button>
